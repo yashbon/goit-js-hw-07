9275706d67fc800ca79a186af0ba2e242846715a
yashbon <104573777+yashbon@users.noreply.github.com>
2023-04-03 03:22:16 +0300

    Completed task - 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,6 +1,5 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
 
 // My code
@@ -14,30 +13,11 @@ const markup = galleryItems.map(
             </a>
         </li>`
 ).join('');
-
 galleryList.insertAdjacentHTML('afterbegin', markup);
 
-galleryList.addEventListener('click', selectPicture);
-
-function selectPicture(event) {
-    event.preventDefault();
-    if (!event.target.classList.contains('gallery__image')) {
-        return;
-    }
-
-    urlImage = event.target.closest('.gallery__link').href;
-    // console.log(urlImage);
-
-    // const instance = basicLightbox.create(`
-    //     <img src="${urlImage}">
-
-    // `);
-    let lightbox = new SimpleLightbox('.gallery .gallery__link', { /* options */
-        captionsData: "hello",
-    });
-
-    lightbox.on('show.simplelightbox', function () {
-        // some usefull information
-
-    });
-}
+// let lightbox = 
+new SimpleLightbox('.gallery__item .gallery__link', { // here is a question!!!
+    /* options */
+    captionsData: 'alt',
+    captionDelay: 250,
+});
